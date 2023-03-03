f515124ed726c28a08a15525ec0f61b0ea3bf642
Giuseppe Albrizio <g.albrizio@gmail.com>
2023-03-03 15:16:35 +0100

    Update typedoc.route.ts

--- src/api/v1/typedoc/typedoc.route.ts
+++ src/api/v1/typedoc/typedoc.route.ts
@@ -1,12 +1,11 @@
 import express from 'express';
 import path from 'path';
-import {devlopmentApiLimiter} from '../../../middlewares/apiRateLimit.middleware';
 
 const typedocRouter = express.Router();
 
 typedocRouter.use(express.static(path.join(__dirname, '../../../../docs')));
 
-typedocRouter.get('/typedoc', devlopmentApiLimiter, (req, res) => {
+typedocRouter.get('/typedoc', (req, res) => {
   res.sendFile(path.join(__dirname, '../../../../docs/index.html'));
 });
 
